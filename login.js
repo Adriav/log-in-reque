35566bf9396f1776885749d615db77d1e2d929ca
Adrian <38671323+Adriav@users.noreply.github.com>
2021-03-20 18:50:27 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,49 @@
-const formlog = document.getElementById("login-form");
-const BtnLog = document.getElementById("login-form-submit");
-const ErrorMessage = document.getElementById("login-error-msg");
-BtnLog.addEventListener("click", (e) =&gt; {
-e.preventDefault();
-const username = formlog.username.value;
-const password = formlog.password.value;
-if (username === "admin" &amp;&amp; password === "itsourcecode") { alert("You have successfully logged in."); location.reload(); } else { ErrorMessage.style.opacity = 1; }
-})
+// Primer codigo JS
+function comenzar(){
+    var boton = document.getElementById("grabar");
+    boton.addEventListener("click", itemNuevo, false);
+
+}
+
+function itemNuevo(){
+    var clave = document.getElementById("clave").value;
+    var valor = document.getElementById("valor").value;
+
+    sessionStorage.setItem(clave, valor);
+    leer(clave);
+
+    document.getElementById("clave").value = "";
+    document.getElementById("valor").value = "";
+}
+
+function leer(clave){
+    var zonadatos = document.getElementById("zonadatos");
+
+    zonadatos.innerHTML='<div><button onclick="eliminarT()">Eliminar Todo</button></div>';
+
+    //zonadatos.innerHTML="";
+
+    for(i=0; i< sessionStorage.length; i++){
+        var clave = sessionStorage.key(i);
+        var elvalor = sessionStorage.getItem(clave);
+        zonadatos.innerHTML += '<div>Clave: '+clave+'<br>Valor: '+elvalor+'<br><button onclick="eliminarI(\''+clave+'\')">Eliminar</button></div>';
+    }
+    
+}
+
+function eliminarT(){
+    if(confirm("Estas seguro?")){
+        sessionStorage.clear();
+        leer();
+    }
+}
+
+function eliminarI(clave){
+    if(confirm("estas seguro?")){
+        sessionStorage.removeItem(clave);
+    }
+    leer();
+}
+
+
+window.addEventListener("load", comenzar, false);
